b48bcdd0d96795055bbce51f6097cde5e21410e7
glitterneey <91388336+GLITTERneey@users.noreply.github.com>
2021-10-31 03:57:17 +0700

    Update ytsearch.py

--- program/ytsearch.py
+++ program/ytsearch.py
@@ -23,7 +23,7 @@ async def ytsearch(_, message: Message):
         [
             [
                 InlineKeyboardButton(
-                    "🗑 Close", callback_data="cls",
+                    "🗑 𝐂𝐥𝐨𝐬𝐞𝐝", callback_data="cls",
                 )
             ]
         ]
@@ -34,15 +34,15 @@ async def ytsearch(_, message: Message):
             await message.reply_text("/search **needs an argument !**")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("🔎 **Searching...**")
+        m = await message.reply_text("🔎 **𝐒𝐞𝐚𝐫𝐜𝐡𝐢𝐧𝐠...**")
         results = YoutubeSearch(query, max_results=5).to_dict()
         i = 0
         text = ""
         while i < 5:
-            text += f"🏷 **Name:** __{results[i]['title']}__\n"
-            text += f"⏱ **Duration:** `{results[i]['duration']}`\n"
-            text += f"👀 **Views:** `{results[i]['views']}`\n"
-            text += f"📣 **Channel:** {results[i]['channel']}\n"
+            text += f"🏷 **𝐍𝐚𝐦𝐞:** __{results[i]['title']}__\n"
+            text += f"⏱ **𝐃𝐮𝐫𝐚𝐭𝐢𝐨𝐧:** `{results[i]['duration']}`\n"
+            text += f"👀 **𝐕𝐢𝐞𝐰𝐬:** `{results[i]['views']}`\n"
+            text += f"📣 **𝐂𝐡𝐚𝐧𝐧𝐞𝐥:** {results[i]['channel']}\n"
             text += f"🔗: https://www.youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, reply_markup=keyboard, disable_web_page_preview=True)
